f84e3e6b7d384f9a1e73e5f07d67d9281a0e1f96
Samara Gideon <sgideon1@yahoo.com>
2019-04-02 10:48:37 -0700

    JS

---
+++ assets/Javascript/JavaScript.js
@@ -0,0 +1,43 @@
+$(document).ready(function() {
+
+    window.onload = function() {
+        alert("Welcome!");
+    };
+
+    $("#Log-In").click(function() {
+        alert("Button Clicked");
+    }
+
+    // document.getElementById("Log-In").addEventListener("click", function() {
+    
+    // })
+
+    // document.getElementById("Sign-Up").addEventListener("click", function() {
+
+    // })
+
+    // document.getElementById("appetizers").addEventListener("click", function() {
+    //     alert("lets get cookin'");
+    // })
+
+    // document.getElementById("breakfast").addEventListener("click", function() {
+
+    // })
+
+    // document.getElementById("brunch").addEventListener("click", function() {
+
+    // })
+
+    // document.getElementById("lunch").addEventListener("click", function() {
+
+    // })
+
+    // document.getElementById("dinner").addEventListener("click", function() {
+
+    // })
+
+    // document.getElementById("dessert").addEventListener("click", function() {
+
+    // })
+
+});
